a9a8c06f95bb7e0f1dfaca36e7eba16b2e801214
juanpsantana@me.com <sagj960928>
2015-03-20 14:46:11 -0600

    BigInteger

---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+using namespace std;
+
+int main(){
+	string num = to_string(5623);
+	return 0
+}
---
+++ yosoy196/yosoy196.cpp
@@ -0,0 +1,38 @@
+// Solving Problems with Programming
+// Professor Ken Bauer
+//
+// Your name here
+// Your student# here
+// If you worked with somebody else, you need to put their names here
+
+// Standard libraries
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// `BigIntegerLibrary.hh' includes all of the library headers.
+#include "BigIntegerLibrary.hh"
+
+/*
+ * This function receives a BigInteger n and checks if it is a palindrome
+ * Return true if n is a palindrome, return false otherwise
+ */
+bool is_palindrome(BigInteger n){
+  return true; // You need to fix this, this is not right
+}
+
+/*
+ * This function receives a BigInteger n and returns the value of n plus the inverse of n.
+ */
+BigInteger apply196(BigInteger n){
+  return n; // you need to replace this with code that works
+}
+
+/*
+ * Your main program goes below and should use the two functions above
+ */
+int main() {
+  cout << "Hola mundo" << endl;
+  return 0;
+}
