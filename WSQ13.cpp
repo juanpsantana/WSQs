1fa2448011c63d2c3c2863fa9a7f8e63f2671332
juanpsantana@me.com <sagj960928>
2015-04-07 15:18:10 -0500

    Babylonian

--- WSQ13.cpp
+++ WSQ13.cpp
@@ -2,3 +2,22 @@
 #include <cstdlib>
 using namespace std;
 
+float root(float number){
+	float old;
+	float guess;
+	for (guess=(number/2.0); old!=guess;){
+		cout << guess << endl;
+		old = guess;
+		guess= (guess + (number/guess))/2.0;
+	}
+
+	return guess;
+}
+
+int main(){
+	float number;
+	cout << "Please, write the number to calculate its square root" << endl;
+	cin >> number;
+	cout << "The square root of " << number << " is " << root(number) << endl;
+	return 0;
+}
